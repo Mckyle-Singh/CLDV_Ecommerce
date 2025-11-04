164715b709bbeb7b909df5cd48f6f025555c5758
User Name <mckylesingh71@gmail.com>
2025-11-04 18:49:48 +0200

    Added all required models

---
+++ Models/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CLDV_Ecommerce.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public IdentityUser? User { get; set; }
+
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+
+        public int Quantity { get; set; }
+
+    }
+}
