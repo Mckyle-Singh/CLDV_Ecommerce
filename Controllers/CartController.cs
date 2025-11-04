d06237943c6fe39779ae90997593198225f0d1cf
User Name <mckylesingh71@gmail.com>
2025-11-04 21:19:42 +0200

    Users can now add items to their cart

---
+++ Controllers/CartController.cs
@@ -0,0 +1,70 @@
+﻿using CLDV_Ecommerce.Data;
+using CLDV_Ecommerce.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CLDV_Ecommerce.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public CartController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userId = _userManager.GetUserId(User);
+            var cartItems = await _context.CartItems
+                .Include(ci => ci.Product)
+                .Where(ci => ci.UserId == userId)
+                .ToListAsync();
+
+            return View(cartItems);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Add(int productId)
+        {
+            var userId = _userManager.GetUserId(User);
+            var existingItem = await _context.CartItems
+                .FirstOrDefaultAsync(ci => ci.ProductId == productId && ci.UserId == userId);
+
+            if (existingItem != null)
+            {
+                existingItem.Quantity++;
+            }
+            else
+            {
+                _context.CartItems.Add(new CartItem
+                {
+                    ProductId = productId,
+                    Quantity = 1,
+                    UserId = userId
+                });
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            var item = await _context.CartItems.FindAsync(id);
+            if (item != null)
+            {
+                _context.CartItems.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
